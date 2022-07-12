f93764993d11ab07314097ab9b35f5f4be974dcb
Miao Zhao <miao_zhao@yahoo.com>
2022-07-12 18:24:58 +1000

    Create function `showName`

--- playground-starter/script.js
+++ playground-starter/script.js
@@ -1,1 +1,8 @@
 console.log("Hello, world!");
+
+function showName() {
+  let inputField = document.getElementById("name-text");
+  console.log("inputField", inputField.value);
+  //   console.dir(inputField);
+  alert("Hello, " + inputField.value);
+}
