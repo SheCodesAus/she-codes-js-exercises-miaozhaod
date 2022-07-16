7a124eca89494b4a57b5acd03d8604ea9533c794
Miao Zhao <miao_zhao@yahoo.com>
2022-07-16 10:47:14 +1000

    Update with arrays and loops

--- playground-starter/script.js
+++ playground-starter/script.js
@@ -1,8 +1,25 @@
 console.log("Hello, world!");
 
-function showName() {
-  let inputField = document.getElementById("name-text");
-  console.log("inputField", inputField.value);
-  //   console.dir(inputField);
-  alert("Hello, " + inputField.value);
+// function showName() {
+//   let inputField = document.getElementById("name-text");
+//   console.log("inputField", inputField.value);
+//   //   console.dir(inputField);
+//   alert("Hello, " + inputField.value);
+// }
+
+// let pets = ["Chilli", "Rex", "Roary", "Fleur"];
+// for (let index = 0; index < pets.length; index++) {
+//   console.log("Found index", pets[index]);
+// }
+// pets.pop();
+// pets.push("Bella");
+// console.log("Found pets", pets);
+
+let pets = ["Chilli", "Rex", "Roary", "Fleur", "Bella"];
+for (let index = 0; index < pets.length; index++) {
+  console.log("Found index", pets[index]);
 }
+
+pets.forEach(pet => {
+  console.log(pet);
+});
