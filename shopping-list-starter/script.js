24c274f81e5d681839ab6b35756a3e1a4bee2b43
Miao Zhao <miao_zhao@yahoo.com>
2022-07-16 22:31:14 +1000

    Update shopping list

--- shopping-list-starter/script.js
+++ shopping-list-starter/script.js
@@ -1,3 +1,40 @@
-function addItem() {}
+let shoppingListItems = ["Milk", "Bread", "Cheese"];
 
-function updateItems() {}
+window.onload = function () {
+  updateItems();
+};
+function addItem() {
+  let itemInput = document.getElementById("new-item-text").value;
+  //   let newListItem = document.createElement("li");
+  //   newListItem.innerText = itemInput;
+  //   console.log("test adding", newListItem);
+  //   shoppingList.appendChild(newListItem);
+  if (itemInput) {
+    shoppingListItems.push(itemInput);
+  }
+  console.log("test adding", shoppingListItems);
+  updateItems();
+}
+
+function updateItems() {
+  let shoppingList = document.getElementById("shopping-list-items");
+  shoppingList.innerHTML = "";
+  shoppingListItems.forEach(item => {
+    let listItem = document.createElement("li");
+    listItem.innerText = item;
+    shoppingList.appendChild(listItem);
+  });
+}
+
+function clearItems() {
+  console.log("test clear");
+  shoppingListItems.splice(0, shoppingListItems.length);
+  console.log(shoppingListItems);
+  let shoppingList = document.getElementById("shopping-list-items");
+  shoppingList.innerHTML = "";
+  shoppingListItems.forEach(item => {
+    let listItem = document.createElement("li");
+    listItem.innerText = item;
+    shoppingList.appendChild(listItem);
+  });
+}
